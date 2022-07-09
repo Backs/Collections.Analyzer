093c4881fb63775cab31c6fe3d69813ca66f07e8
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2022-07-09 13:10:56 +0700

    - detect ToArray or ToList calls at string.Join

---
+++ Tests/StringTests/StringJoinToArrayConversionTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Tests.StringTests
+{
+    using System.Threading.Tasks;
+    using Collections.Analyzer;
+    using Microsoft.CodeAnalysis.CSharp.Testing;
+    using Microsoft.CodeAnalysis.Testing;
+    using Microsoft.CodeAnalysis.Testing.Verifiers;
+    using NUnit.Framework;
+
+    public class StringJoinToArrayConversionTests : CSharpCodeFixTest<StringJoinToArrayDiagnostic,
+        RemoveRedundantMethodCallCodeFix, NUnitVerifier>
+    {
+        [Test]
+        public Task StringJoinToArrayTest()
+        {
+            var code = ResourceReader.ReadFromFile("StringJoin1.txt");
+
+            return StringJoinToArrayConversionVerifier
+            .VerifyAnalyzerAsync(code, DiagnosticResult.CompilerWarning("CI0003").WithSpan(11, 44, 11, 78));
+        }
+        
+        [TestCase("StringJoinBefore.txt", "StringJoinAfter.txt")]
+        public Task CodeFixesTest(string before, string after)
+        {
+            var code = ResourceReader.ReadFromFile(before);
+            var fixedCode = ResourceReader.ReadFromFile(after);
+
+            return StringJoinToArrayConversionVerifier.VerifyCodeFixAsync(code, fixedCode);
+        }
+    }
+}
---
+++ Tests/StringTests/StringJoinToArrayConversionVerifier.cs
@@ -0,0 +1,12 @@
+﻿namespace Tests.StringTests
+{
+    using Collections.Analyzer;
+    using Microsoft.CodeAnalysis.Testing;
+    using Microsoft.CodeAnalysis.Testing.Verifiers;
+
+    internal sealed class StringJoinToArrayConversionVerifier : CodeFixVerifier<StringJoinToArrayDiagnostic, RemoveRedundantMethodCallCodeFix,
+        StringJoinToArrayConversionTests,
+        NUnitVerifier>
+    {
+    }
+}
