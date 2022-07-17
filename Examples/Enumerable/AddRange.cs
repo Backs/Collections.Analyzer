508235898a2daab32a26cfe7db075d6b87b6dec9
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2022-07-17 20:54:12 +0700

    examples

---
+++ Examples/Enumerable/AddRange.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Examples.Enumerable
+{
+    public class AddRange
+    {
+        public void Method1()
+        {
+            var list = new List<int>();
+            IEnumerable<int> array = new int[10];
+
+            list.AddRange(array.ToArray());
+        }
+        
+        public void Method2()
+        {
+            var list = new List<int>();
+
+            list.AddRange(GetArray().ToArray());
+        }
+
+        private static IEnumerable<int> GetArray()
+        {
+            return new int[10];
+        }
+    }
+}
