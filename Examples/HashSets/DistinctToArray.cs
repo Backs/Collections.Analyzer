0c6f93750b9c471c48231cc4ba180d88fc51aaf8
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2025-12-13 18:02:24 +0700

    Add CI0008 diagnostic, v0.2.13 (#12)
    * Added CI0008 diagnostic: An array is used with the Contains method for lookup operations.

---
+++ Examples/HashSets/DistinctToArray.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Examples.HashSets;
+
+public class DistinctToArray
+{
+    public void Method1()
+    {
+        var numbers = new[] { 1, 2, 3 };
+        ICollection<int> result = numbers.Distinct().ToArray();
+    }
+}
---
+++ Tests/ArrayContainsToHashSetTests/ArrayContainsVerifier.cs
@@ -0,0 +1,13 @@
+﻿using Collections.Analyzer.CodeFixes;
+using Collections.Analyzer.Diagnostics.CI0008;
+using Microsoft.CodeAnalysis.Testing;
+
+namespace Tests.ArrayContainsToHashSetTests;
+
+public class ArrayContainsVerifier : CodeFixVerifier<
+    ArrayContainsToHashSetDiagnostic,
+    ArrayContainsToHashSetCodeFix,
+    ArrayContainsToHashSetTests,
+    DefaultVerifier>
+{
+}
