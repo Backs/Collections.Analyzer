97c9357d5a8ec747be194966a4ec85e2a7807746
Sergey Rogatnev <rogatnev.sergey@gmail.com>
2026-01-07 13:02:31 +0700

    extended capacity test

--- Collections.Analyzer/CodeFixes/SetListCapacityCodeFix.cs
+++ Collections.Analyzer/CodeFixes/SetListCapacityCodeFix.cs
@@ -21,51 +21,58 @@ namespace Collections.Analyzer.CodeFixes
         public override ImmutableArray<string> FixableDiagnosticIds => ImmutableArray.Create(
             ListInitializerDiagnostic.InitializeListWithCapacityRule.Id);
 
+        public override FixAllProvider GetFixAllProvider() => WellKnownFixAllProviders.BatchFixer;
+
         public override async Task RegisterCodeFixesAsync(CodeFixContext context)
         {
-            var root = await context.Document.GetSyntaxRootAsync();
+            var root = await context.Document.GetSyntaxRootAsync(context.CancellationToken).ConfigureAwait(false);
             var diagnostic = context.Diagnostics.First();
-            var objectCreationExpression = (ObjectCreationExpressionSyntax)root!.FindNode(context.Span);
+            var span = context.Span;
+
+            var objectCreation = root!.FindNode(span).FirstAncestorOrSelf<BaseObjectCreationExpressionSyntax>();
+
+            if (objectCreation == null)
+                return;
 
             context.RegisterCodeFix(
                 CodeAction.Create(
                     Resources.SetActualCapacity,
-                    token => FixAsync(context.Document, objectCreationExpression, token),
-                    "Set capacity"
+                    token => FixAsync(context.Document, objectCreation, token),
+                    nameof(SetListCapacityCodeFix)
                 ),
                 diagnostic
             );
         }
 
         private static async Task<Document> FixAsync(Document document,
-            ObjectCreationExpressionSyntax objectCreationExpression,
+            BaseObjectCreationExpressionSyntax objectCreation,
             CancellationToken cancellationToken)
         {
-            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken);
-            var capacity = objectCreationExpression.Initializer!.Expressions.Count;
-            var genericNameSyntax = (GenericNameSyntax) objectCreationExpression.Type;
+            var oldRoot = await document.GetSyntaxRootAsync(cancellationToken).ConfigureAwait(false);
+            
+            var count = objectCreation.Initializer?.Expressions.Count ?? 0;
 
-            var newObject = ObjectCreationExpression(
-                    GenericName(genericNameSyntax.Identifier)
-                        .WithTypeArgumentList(genericNameSyntax.TypeArgumentList))
-                .WithArgumentList(
-                    ArgumentList(
-                        SingletonSeparatedList(
-                            Argument(
-                                LiteralExpression(
-                                    SyntaxKind.NumericLiteralExpression,
-                                    Literal(capacity))))))
-                .WithInitializer(objectCreationExpression.Initializer);
+            var newArgumentList = ArgumentList(
+                SingletonSeparatedList(
+                    Argument(LiteralExpression(SyntaxKind.NumericLiteralExpression, Literal(count)))));
 
-            var newRoot = oldRoot!.ReplaceNode(objectCreationExpression, newObject);
+            BaseObjectCreationExpressionSyntax newObjectCreation;
+
+            switch (objectCreation)
+            {
+                case ImplicitObjectCreationExpressionSyntax implicitCreation:
+                    newObjectCreation = implicitCreation.WithArgumentList(newArgumentList);
+                    break;
+                case ObjectCreationExpressionSyntax explicitCreation:
+                    newObjectCreation = explicitCreation.WithArgumentList(newArgumentList);
+                    break;
+                default:
+                    return document;
+            }
+
+            var newRoot = oldRoot!.ReplaceNode(objectCreation, newObjectCreation);
 
             return document.WithSyntaxRoot(newRoot);
-        }
-
-
-        public override FixAllProvider GetFixAllProvider()
-        {
-            return WellKnownFixAllProviders.BatchFixer;
         }
     }
 }
---
+++ Examples/Lists/ListCapacity.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+namespace Examples.Lists;
+
+public class ListCapacity
+{
+    private readonly List<int> list1 = new() { 1, 2, 3 };
+    private List<int> list2 = new List<int> { 1, 2, 3 };
+
+    public List<int> Method1()
+    {
+        return new List<int> { 1, 2, 3 };
+    }
+
+    public List<int> Method2()
+    {
+        List<int> list = new() { 1, 2, 3 };
+        return list;
+    }
+    
+    public List<int> Method3()
+    {
+        return new List<int>(1) { 1, 2, 3 };
+    }
+    
+    public List<int> Method4()
+    {
+        const int capacity = 2;
+        return new List<int>(capacity) { 1, 2, 3 };
+    }
+}
